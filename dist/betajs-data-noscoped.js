31fe26dc685fb77f6144e54f5e2a663bc0a52798
Oliver Friedmann <oliver.friedmann@gmail.com>
2019-01-02 11:39:09 -0500

    Merge

--- dist/betajs-data-noscoped.js
+++ dist/betajs-data-noscoped.js
@@ -1,5 +1,5 @@
 /*!
-betajs-data - v1.0.133 - 2018-12-09
+betajs-data - v1.0.134 - 2019-01-02
 Copyright (c) Oliver Friedmann
 Apache-2.0 Software License.
 */
@@ -11,8 +11,8 @@ Scoped.binding('base', 'global:BetaJS');
 Scoped.define("module:", function () {
 	return {
     "guid": "70ed7146-bb6d-4da4-97dc-5a8e2d23a23f",
-    "version": "1.0.133",
-    "datetime": 1544401178983
+    "version": "1.0.134",
+    "datetime": 1546447065112
 };
 });
 Scoped.assumeVersion('base:version', '~1.0.141');
@@ -8403,6 +8403,56 @@ Scoped.define("module:Modelling.Validators.LengthValidator", [
                 return resp;
             }
 
+        };
+    });
+});
+Scoped.define('module:Modelling.Validators.MinMaxValidator', [
+    'module:Modelling.Validators.Validator',
+    'base:Types',
+    'base:Objs'
+], function(Validator, Types, Objs, scoped) {
+    return Validator.extend({
+        scoped: scoped
+    }, function(inherited) {
+        return {
+            constructor: function(options) {
+                inherited.constructor.call(this);
+                options = Objs.extend({
+                    min_value: null,
+                    max_value: null,
+                    error_string: null
+                }, options);
+                if (options.min_value && !Types.isNumber(options.min_value))
+                    throw new Error('Min value must be a number.');
+                if (options.max_value && !Types.isNumber(options.max_value))
+                    throw new Error('Max value must be a number.');
+                this.__min_value = options.min_value;
+                this.__max_value = options.max_value;
+                this.__error_string = options.error_string;
+                if (!this.__error_string) {
+                    if (this.__min_value !== null) {
+                        if (this.__max_value !== null) {
+                            this.__error_string = 'Between ' + this.__min_value + ' and ' + this.__max_value +
+                                ' characters';
+                        } else {
+                            this.__error_string = 'At least ' + this.__min_value;
+                        }
+                    } else if (this.__max_value !== null)
+                        this.__error_string = 'At most ' + this.__max_value;
+                }
+            },
+
+            validate: function(value, context, key) {
+                if (!Types.isNumber(value))
+                    throw new Error('MinMax Validator is for numbers only.');
+                if (this.__min_value !== null && (!value || (value < this.__min_value))) {
+                    return this.__error_string;
+                }
+                if (this.__max_value !== null && (value && (value > this.__max_value))) {
+                    return this.__error_string;
+                }
+                return null;
+            }
         };
     });
 });
